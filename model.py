3fd389a04783b1e28ea46cdd7bacc0cde106b431
EstebanHernandez0 <EstebanHernandez2024@gmail.com>
2023-02-16 21:34:12 -0600

    Update

---
+++ model.py
@@ -0,0 +1,53 @@
+# transform
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+import statsmodels.api as sm
+from statsmodels.tsa.api import Holt, ExponentialSmoothing
+np.random.seed(0)
+
+from sklearn.metrics import mean_squared_error
+from math import sqrt 
+
+
+
+def evaluate(target_var):
+    '''
+    This function will take the actual values of the target_var from validate, 
+    and the predicted values stored in yhat_df, 
+    and compute the rmse, rounding to 0 decimal places. 
+    it will return the rmse. 
+    '''
+    rmse = round(sqrt(mean_squared_error(validate[target_var], yhat_df[target_var])), 0)
+    return rmse
+
+def plot_and_eval(target_var):
+    '''
+    This function takes in the target var name (string), and returns a plot
+    of the values of train for that variable, validate, and the predicted values from yhat_df. 
+    it will als lable the rmse. 
+    '''
+    plt.figure(figsize = (12,4))
+    plt.plot(train[target_var], label='Train', linewidth=1, color='#377eb8')
+    plt.plot(val[target_var], label='Validate', linewidth=1, color='#ff7f00')
+    plt.plot(yhat_df[target_var], label='yhat', linewidth=2, color='#a65628')
+    plt.legend()
+    plt.title(target_var)
+    rmse = evaluate(target_var)
+    print(target_var, '-- RMSE: {:.0f}'.format(rmse))
+    plt.show()
+
+
+    # function to store the rmse so that we can compare
+def append_eval_df(model_type, target_var):
+    '''
+    this function takes in as arguments the type of model run, and the name of the target variable. 
+    It returns the eval_df with the rmse appended to it for that model and target_var. 
+    '''
+    rmse = evaluate(target_var)
+    d = {'model_type': [model_type], 'target_var': [target_var],
+        'rmse': [rmse]}
+    d = pd.DataFrame(d)
+    return eval_df.append(d, ignore_index = True)
